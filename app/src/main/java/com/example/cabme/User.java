0d6d7262b725c13e4ec908bd519b8c44cc04badf
Owen Johnson <ojohnson@ualberta.ca>
2020-03-03 00:36:26 -0700

    Revert "Revert "Merge branch 'ojohnson' into ridwan1""
    This reverts commit 903099b52e3c8787db5e0c56a15d1a7920cabb27.

--- app/src/main/java/com/example/cabme/User.java
+++ app/src/main/java/com/example/cabme/User.java
@@ -1,58 +1,257 @@
-package com.example.cabme;
+import android.util.Log;
 
-public class User {
-	private String firstName;
-	private String lastName;
-	private String email;
-	private String userName;
-	private String phone;
-	private String password;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-	User (String firstName,String lastName, String email, String userName, String phone, String password) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.userName = userName;
-		this.phone = phone;
-		this.password = password;
-	}
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
-	public String getFirstName() {
-		return this.firstName;
-	}
-	public String getLastName() {
-		return this.lastName;
-	}
-	public String getEmail() {
-		return this.email;
-	}
-	public String getUserName() {
-		return this.userName;
-	}
-	public String getPhone() {
-		return this.phone;
-	}
-	public String getPassword() {
-		return this.password;
-	}
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ *  User class meant to pull and provide information to the FireBase database about the users
+ *  involved in the CabMe application
+ */
+public class User extends CModel implements Serializable {
+    final  private String TAG = "User";
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String uid;
+    private String phone;
+    //private int balance;
+    private transient FirebaseFirestore db;
+    private transient CollectionReference collectionReference;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    /**
+     * This constructor is for users that are logging in or getting information of other
+     * user involved in the current request
+     *
+     * @param uid
+     */
+    User (String uid) {
+        db = FirebaseFirestore.getInstance();
+        collectionReference = db.collection("users");
+        this.uid = uid;
+
+        collectionReference
+                .document(uid)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Log.d(TAG, "Data retrieval successful");
+
+                        email = documentSnapshot.getString("email");
+                        firstName = documentSnapshot.getString("first");
+                        lastName = documentSnapshot.getString("last");
+                        username = documentSnapshot.getString("username");
+                        phone = documentSnapshot.getString("phone");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Data retrieval failed " + e.toString());
+                    }
+                });
+        setDocumentListener(uid);
+    }
+
+    /**
+     * This constructor is for users that are signing up for the CabMe application
+     *
+     * @param uid
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @param username
+     * @param phone
+     */
+    User (String uid, String email, String firstName, String lastName, String username, String phone) {
+
+        db = FirebaseFirestore.getInstance();
+        collectionReference = db.collection("users");
+        this.uid = uid;
+
+        Map<String, Object> userData = new HashMap<>();
+        userData.put("email", email);
+        userData.put("first", firstName);
+        userData.put("last", lastName);
+        userData.put("username", username);
+        userData.put("phone", phone);
+
+        collectionReference
+                .document(uid)
+                .set(userData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Data addition successful");
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Data addition failed "+ e.toString());
+                    }
+                });
+
+        setDocumentListener(uid);
+
+    }
+
+    /**
+     * This method sets a listener to the user's document in the database to retrieve real-time
+     * updates from the database
+     *
+     * @param uid
+     */
+    public void setDocumentListener(String uid) {
+        collectionReference.document(uid).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (documentSnapshot == null) {
+                    return;
+                }
+
+                username = documentSnapshot.getString("username");
+                firstName = documentSnapshot.getString("first");
+                lastName = documentSnapshot.getString("last");
+                email = documentSnapshot.getString("email");
+                phone = documentSnapshot.getString("phone");
+
+                notifyViews();
+            }
+        });
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setEmail(String email) {
+        collectionReference
+                .document(uid)
+                .update("email", email)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Email update successful");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Email update failed "+ e.toString());
+                    }
+                });
+    }
+
+    public void setPhone(String phone) {
+        collectionReference
+                .document(uid)
+                .update("phone", phone)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Phone update successful");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Phone update failed "+ e.toString());
+                    }
+                });
+    }
+
+    public void setUsername(String username) {
+        collectionReference
+                .document(uid)
+                .update("username", username)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Username update successful");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Username update failed "+ e.toString());
+                    }
+                });
+    }
+
+    public void setFirstName(String firstName) {
+        collectionReference
+                .document(uid)
+                .update("first", firstName)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "First name update successful");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "First name update failed "+ e.toString());
+                    }
+                });
+    }
+
+    public void setLastName(String lastName) {
+        collectionReference
+                .document(uid)
+                .update("last", lastName)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Last name update successful");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Last name update failed "+ e.toString());
+                    }
+                });
+    }
+
+    //public int getBalance() {
+    //    return balance;
+    //}
+
 }
