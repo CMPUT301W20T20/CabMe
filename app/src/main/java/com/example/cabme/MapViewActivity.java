74017fdbc737ad73107512d7390d3e2fc9d71fd6
Justine Zapansa <patriciazapansa@gmail.com>
2020-03-12 15:22:57 -0600

    added googles routes thing

--- app/src/main/java/com/example/cabme/MapViewActivity.java
+++ app/src/main/java/com/example/cabme/MapViewActivity.java
@@ -1,9 +1,12 @@
 package com.example.cabme;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,16 +18,36 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
-public class MapViewActivity extends FragmentActivity implements OnMapReadyCallback{
-    private MapView mapView;
-    private GoogleMap googleMap;
+public class MapViewActivity extends FragmentActivity implements OnMapReadyCallback, TaskLoadedCallback{
     Button driverButton;
+    Button backButton;
+
+    GoogleMap googleMap;
+
+    // Directions
+    LongLat destLngLat;
+    LongLat startLngLat;
+    LatLng startLatLng;
+    LatLng destLatLng;
+
+    // Drawing Lines & Markers
+    Polyline currPolyline;
+    MarkerOptions markStart;
+    MarkerOptions markDest;
 
     /**
      * Checks for the bundle.
@@ -35,25 +58,30 @@ public class MapViewActivity extends FragmentActivity implements OnMapReadyCallb
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.r_mapview_activity);
-    }
 
-    /**
-     * Making a callback function for when the map object is ready.
-     * As the map is read,
-     * The onMapReady function is called to go throught the given MoodEvent.
-     *
-     * @param inflater
-     * @param container
-     * @param savedInstanceState
-     * @return
-     */
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.r_mapview_activity, container, false);
         driverButton = findViewById(R.id.driver_name);
-        mapView = findViewById(R.id.map);
+        backButton = findViewById(R.id.back_button);
+
+        destLngLat = (LongLat) getIntent().getSerializableExtra("destLongLat");
+        startLngLat = (LongLat) getIntent().getSerializableExtra("startLongLat");
+
+        startLatLng = new LatLng(startLngLat.getLat(), startLngLat.getLng());
+        destLatLng = new LatLng(destLngLat.getLat(), destLngLat.getLng());
+
+        markStart = new MarkerOptions().position(startLatLng).title("Start Location");
+        markDest = new MarkerOptions().position(destLatLng).title("Destination Location");
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        return view;
+        mapFragment.getMapAsync(MapViewActivity.this);
+
+        new FetchURL(MapViewActivity.this)
+                .execute(getUrl(markStart.getPosition(), markDest.getPosition(), "driving"), "driving");
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MapViewActivity.this, NewRideInfoActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 
     /**
@@ -65,13 +93,51 @@ public class MapViewActivity extends FragmentActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap map) {
         googleMap = map;
         setUpMap(map);
+
     }
 
     public void setUpMap(GoogleMap map){
-//        LatLng startLatLng = new LatLng(53.55014, -113.46871);
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        map.clear();
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(startLatLng);
+        builder.include(destLatLng);
+
+        addMarkers(map, startLatLng, destLatLng);
+
+        map.animateCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 10));
     }
 
+    public void addMarkers(GoogleMap map, LatLng start, LatLng dest){
+        Log.wtf("MVA Start", "StartLatLng: "+ startLngLat.getLat() + "\n" + startLngLat.getLng());
+        Log.wtf("MVA Dest", "StartLatLng: "+ startLngLat.getLat() + "\n" + startLngLat.getLng());
+        map.addMarker(markStart);
+        map.addMarker(markDest);
+    }
+
+    private String getUrl(LatLng origin, LatLng dest, String directionMode) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Mode
+        String mode = "mode=" + directionMode;
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + mode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "&key=" + getString(R.string.google_maps_key);
+        return url;
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currPolyline != null)
+            currPolyline.remove();
+        currPolyline = googleMap.addPolyline((PolylineOptions) values[0]);
+    }
 
 }
+
+
