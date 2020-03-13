14bd983309ccdc8d6558450eb6058616ef082edf
Ridwan Hasan <mdridwan@ualberta.ca>
2020-03-12 19:42:12 -0600

    unique username signup

--- app/src/main/java/com/example/cabme/SignupActivity.java
+++ app/src/main/java/com/example/cabme/SignupActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +17,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -55,62 +59,68 @@ public class SignupActivity extends AppCompatActivity {
 		db = FirebaseFirestore.getInstance();
 		mauth = FirebaseAuth.getInstance();
 
-		final CollectionReference collectionReference = db.collection("users");
 
 		signupButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				if (validate()) {
+				fname = firstNameEditText.getText().toString().trim();
+				lname = lastNameEditText.getText().toString().trim();
+				email = emailEditText.getText().toString().trim();
+				phone = phoneEditText.getText().toString().trim();
+				uname = userNameEditText.getText().toString().trim();
+				pass = passwordEditText.getText().toString();
+				repass = repasswordEditText.getText().toString();
+				final CollectionReference collectionReference = db.collection("users");
 
-					final String sfname = firstNameEditText.getText().toString().trim();
-					final String slname = lastNameEditText.getText().toString().trim();
-					final String semail = emailEditText.getText().toString().trim();
-					final String sphone = phoneEditText.getText().toString().trim();
-					final String suname = userNameEditText.getText().toString().trim();
-					final String spass = passwordEditText.getText().toString();
-					final String srepass = repasswordEditText.getText().toString();
-
-					mauth.createUserWithEmailAndPassword(semail,spass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-						@Override
-						public void onComplete(@NonNull Task<AuthResult> task) {
-
-							if (task.isSuccessful()) {
-								Toast.makeText(SignupActivity.this, "Successfully Registered, Upload complete!", Toast.LENGTH_SHORT).show();
-								String uid = mauth.getCurrentUser().getUid();
-								user = new User(uid,semail, sfname, slname, suname, sphone);
-								finish();
-								//startActivity(new Intent(MainActivity.this, TitleActivity.class));
-							} else {
-								FirebaseAuthException e = (FirebaseAuthException) task.getException();
-								String s = "Sign up Failed" + task.getException();
-								Toast.makeText(SignupActivity.this, s, Toast.LENGTH_LONG).show();
-								return;
+				Query query = collectionReference.whereEqualTo("username",uname);
+				query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+					@Override
+					public void onComplete(@NonNull Task<QuerySnapshot> task) {
+						if(task.isSuccessful()){
+							for(DocumentSnapshot documentSnapshot: task.getResult()){
+								String user = documentSnapshot.getString("username");
+								if(user.equals(uname)){
+									Log.d(TAG,"User Exists");
+									Toast.makeText(SignupActivity.this, "Please choose a unique username", Toast.LENGTH_LONG).show();
+								}
 							}
-
 						}
-					});
-				}
+						if(task.getResult().size()==0){
+							Log.d(TAG,"User does not Exist");
+							if (fname.isEmpty() || lname.isEmpty() || email.isEmpty() || uname.isEmpty() || pass.isEmpty() || repass.isEmpty() || phone.isEmpty()) {
+								Toast.makeText(SignupActivity.this, "Please enter all the details", Toast.LENGTH_SHORT).show();
+							} else {
+								final String sfname = firstNameEditText.getText().toString().trim();
+								final String slname = lastNameEditText.getText().toString().trim();
+								final String semail = emailEditText.getText().toString().trim();
+								final String sphone = phoneEditText.getText().toString().trim();
+								final String suname = userNameEditText.getText().toString().trim();
+								final String spass = passwordEditText.getText().toString();
+								final String srepass = repasswordEditText.getText().toString();
+
+								mauth.createUserWithEmailAndPassword(semail,spass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+									@Override
+									public void onComplete(@NonNull Task<AuthResult> task) {
+
+										if (task.isSuccessful()) {
+											Toast.makeText(SignupActivity.this, "Successfully Registered, Upload complete!", Toast.LENGTH_SHORT).show();
+											String uid = mauth.getCurrentUser().getUid();
+											user = new User(uid,semail, sfname, slname, suname, sphone);
+											finish();
+											//startActivity(new Intent(MainActivity.this, TitleActivity.class));
+										} else {
+											FirebaseAuthException e = (FirebaseAuthException) task.getException();
+											String s = "Sign up Failed" + task.getException();
+											Toast.makeText(SignupActivity.this, s, Toast.LENGTH_LONG).show();
+											return;
+										}
+									}
+								});
+							}
+						}
+					}
+				});
 			}
 		});
-	}
-
-	private Boolean validate() {
-		Boolean result = false;
-
-		fname = firstNameEditText.getText().toString().trim();
-		lname = lastNameEditText.getText().toString().trim();
-		email = emailEditText.getText().toString().trim();
-		phone = phoneEditText.getText().toString().trim();
-		uname = userNameEditText.getText().toString().trim();
-		pass = passwordEditText.getText().toString();
-		repass = repasswordEditText.getText().toString();
-
-
-		if (fname.isEmpty() || lname.isEmpty() || email.isEmpty() || uname.isEmpty() || pass.isEmpty() || repass.isEmpty() || phone.isEmpty()) {
-			Toast.makeText(this, "Please enter all the details", Toast.LENGTH_SHORT).show();
-		} else {
-			result = true;
-		}
-		return result;
 	}
 }
