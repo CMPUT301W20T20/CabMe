0cfd01555b25ec3c4b007edb6fcd61467607b1c7
Justine Zapansa <39612017+zapansa@users.noreply.github.com>
2020-03-13 22:20:06 -0600

    Revert "Zapansa3"

--- app/src/main/java/com/example/cabme/ProfileActivity.java
+++ app/src/main/java/com/example/cabme/ProfileActivity.java
@@ -1,39 +1,21 @@
 package com.example.cabme;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
 public class ProfileActivity extends AppCompatActivity implements Observer {
-    private String TAG = "PROFILE";
     private Button saveButton;
     private Button backButton;
     private Button editButton;
@@ -50,8 +32,6 @@ public class ProfileActivity extends AppCompatActivity implements Observer {
     private String nfname;
     private Map<String, Object> data;
     private User user;
-    private FirebaseAuth mauth;
-    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -60,11 +40,6 @@ public class ProfileActivity extends AppCompatActivity implements Observer {
         user = (User)getIntent().getSerializableExtra("user");
         data = new HashMap<>();
         user.addObserver(this);
-        mauth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
-        CollectionReference collectionReference = db.collection("users");
-        user.setDocumentListener();
-
 
         saveButton = findViewById(R.id.saveprofile);
         backButton = findViewById(R.id.back);
@@ -104,29 +79,6 @@ public class ProfileActivity extends AppCompatActivity implements Observer {
             }
         });
 
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
-                builder
-                        .setTitle("DELETE PROFILE")
-                        .setMessage("Are you sure you wish to delete your profile")
-                        .setNegativeButton("Cancel", null)
-                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                mauth.getCurrentUser().delete();
-                                collectionReference.document(user.getUid()).delete();
-                                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |  Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                                startActivity(intent);
-                            }
-                        })
-                        .show();
-
-            }
-        });
-
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -135,64 +87,39 @@ public class ProfileActivity extends AppCompatActivity implements Observer {
                 nusername = usernameEditText.getText().toString();
                 nlname = lnameEditText.getText().toString();
                 nfname = fnameEditText.getText().toString();
-                Query query = collectionReference.whereEqualTo("username",nusername);
-                Log.d(TAG, user.getUsername());
+                if (true) { //valid(nemail, nphone, nusername, nlname, nfname)
 
-                if (valid(nemail, nusername, nphone, nlname, nfname)) {
-                    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if(task.isSuccessful()){
-                                if (!task.getResult().isEmpty() && !user.getUsername().equals(nusername)) {
-                                    Toast.makeText(ProfileActivity.this, "Username is taken", Toast.LENGTH_LONG).show();
-                                } else {
-                                    mauth.getCurrentUser().updateEmail(nemail)
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    if (task.isSuccessful()) {
-                                                        data.put("email", nemail);
-                                                        if (!user.getPhone().equals(nphone)) {
-                                                            data.put("phone", nphone);
-                                                        }
-                                                        if (!user.getUsername().equals(nusername)) {
-                                                            data.put("username", nusername);
-                                                        }
-                                                        if (!user.getLastName().equals(nlname)) {
-                                                            data.put("last", nlname);
-                                                        }
-                                                        if (!user.getFirstName().equals(nfname)) {
-                                                            data.put("first", nfname);
-                                                        }
-
-                                                        user.updateData(data);
-
-                                                        emailEditText.setEnabled(false);
-                                                        phoneEditText.setEnabled(false);
-                                                        usernameEditText.setEnabled(false);
-                                                        lnameEditText.setEnabled(false);
-                                                        fnameEditText.setEnabled(false);
-
-                                                        saveButton.setVisibility(View.GONE);
-                                                        deleteButton.setVisibility(View.GONE);
-                                                        editButton.setVisibility(View.VISIBLE);
-
-                                                    } else {
-                                                        Toast.makeText(ProfileActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                                                    }
-                                                }
-                                            });
-                                }
-                            } else {
-                                Toast.makeText(ProfileActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
+                    if (!user.getEmail().equals(nemail)) {
+                        data.put("email", nemail);
+                    }
+                    if (!user.getPhone().equals(nphone)) {
+                        data.put("phone", nphone);
+                    }
+                    if (!user.getUsername().equals(nusername)) {
+                        data.put("username", nusername);
+                    }
+                    if (!user.getLastName().equals(nlname)) {
+                        data.put("last", nlname);
+                    }
+                    if (!user.getFirstName().equals(nfname)) {
+                        data.put("first", nfname);
+                    }
 
 
+                    user.updateData(data);
 
 
+                    emailEditText.setEnabled(false);
+                    phoneEditText.setEnabled(false);
+                    usernameEditText.setEnabled(false);
+                    lnameEditText.setEnabled(false);
+                    fnameEditText.setEnabled(false);
+
+                    saveButton.setVisibility(View.GONE);
+                    deleteButton.setVisibility(View.GONE);
+                    editButton.setVisibility(View.VISIBLE);
                 }
+
 
 
             }
@@ -211,32 +138,5 @@ public class ProfileActivity extends AppCompatActivity implements Observer {
         }
 
     }
-
-    private boolean valid(String email, String username, String phone, String lastname, String firstname) {
-        boolean valid = true;
-        String error = "";
-        if (email.isEmpty()) {
-            error += "Email field is empty \n";
-            valid = false;
-        }
-        if (username.isEmpty()) {
-            error += "Email field is empty \n";
-            valid = false;
-        } if (phone.isEmpty()) {
-            error += "Phone number field is empty \n";
-            valid = false;
-        } if (lastname.isEmpty()) {
-            error += "Last name field is empty \n";
-            valid = false;
-        } if (firstname.isEmpty()) {
-            error += "First name field is empty \n";
-            valid = false;
-        }
-
-        if (!valid) {
-            Toast.makeText(ProfileActivity.this, error, Toast.LENGTH_LONG).show();
-        }
-        return valid;
-    }
-
+    
 }
