4056a2bfd3eefd31edff5c598175932aa293dc2f
Shridhar Patel <shridhar.patel@ymail.com>
2020-03-13 21:10:45 -0600

    adding appropriate manifest

--- app/src/main/java/com/example/cabme/ImageDownloaderClass.java
+++ app/src/main/java/com/example/cabme/ImageDownloaderClass.java
@@ -1,4 +1,4 @@
-package com.example.qrcode;
+package com.example.cabme;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
--- app/src/main/java/com/example/cabme/ScannerQR.java
+++ app/src/main/java/com/example/cabme/ScannerQR.java
@@ -1,4 +1,4 @@
-package com.example.qrcode;
+package com.example.cabme;
 
 import android.Manifest;
 import android.os.Bundle;
