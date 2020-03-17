56dd113a326b0262d3fc3aedd9bf4bb2c54b206f
Justine Zapansa <patriciazapansa@gmail.com>
2020-03-17 15:37:16 -0600

    added comments

--- app/src/main/java/com/example/cabme/maps/LongLat.java
+++ app/src/main/java/com/example/cabme/maps/LongLat.java
@@ -2,6 +2,23 @@ package com.example.cabme.maps;
 
 import java.io.Serializable;
 
+/**
+ *
+ * Purposes:
+ * Stores Lat and Lng as doubles
+ *
+ * Params:
+ * - double:: latitude
+ * - double:: longitude
+ *
+ * QUESTIONS:
+ * This isn't needed i just found out about GeoPoints and LatLng objects that basically do the same lol
+ *
+ * TODO:
+ *  [ ] Check if alternatives serializable
+ *  [ ] Delete and switch to using the alternatives instead of this class
+ *
+ */
 public class LongLat implements Serializable {
     Double Lat;
     Double Lng;
