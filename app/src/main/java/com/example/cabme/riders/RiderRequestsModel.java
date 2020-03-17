6d751115dc83f595a7310d6e5bd8e86ca93c11ee
Justine Zapansa <patriciazapansa@gmail.com>
2020-03-17 15:39:42 -0600

    added get/setters

--- app/src/main/java/com/example/cabme/riders/RiderRequestsModel.java
+++ app/src/main/java/com/example/cabme/riders/RiderRequestsModel.java
@@ -1,51 +1,131 @@
 package com.example.cabme.riders;
 
-public class RiderRequestsModel {
+import android.util.Log;
 
-    private String status;
-    private com.google.firebase.firestore.GeoPoint startLocation;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.GeoPoint;
 
-    //private String startLocation;
-    private com.google.firebase.firestore.GeoPoint endLocation;
-    private String driverID;
+import java.util.Observable;
+
+public class RiderRequestsModel extends Observable {
+
+    private String rideStatus;
+    private Double rideCost;
+
+    private GeoPoint startLocation;
+    private GeoPoint endLocation;
+
+    private String startAddress;
+    private String endAddress;
+
+    private String UIDdriver;
+    private String UIDrider;
+
+    private String distanceText;
+    private Integer distanceValue;
+
+    private String durationText;
+    private Integer durationValue;
 
     private RiderRequestsModel(){}
-    private RiderRequestsModel(String status, com.google.firebase.firestore.GeoPoint startLocation, com.google.firebase.firestore.GeoPoint endLocation, String driverID){
+
+    private RiderRequestsModel(
+            String status,
+            GeoPoint startLocation, GeoPoint endLocation,
+            String UIDdriver, String UIDrider,
+            String startAddress, String endAddress,
+            String distanceText, Integer distanceValue,
+            String durationText, Integer durationValue,
+            String rideStatus, Double rideCost){
+
       setStatus(status);
       setStartLocation(startLocation);
       setEndLocation(endLocation);
-      setDriverID(driverID);
+      setUIDdriver(UIDdriver);
+      setUIDrider(UIDrider);
+      setStartAddress(startAddress);
+      setEndAddress(endAddress);
+      setDistanceText(distanceText);
+      setDistanceValue(distanceValue);
+      setDurationText(durationText);
+      setDurationValue(durationValue);
+      setRideCost(rideCost);
+      setRideStatus(rideStatus);
     }
 
     public String getStatus() {
-        return status;
+        return rideStatus;
     }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public com.google.firebase.firestore.GeoPoint getStartLocation() {
-        return startLocation;
-    }
-
-    public void setStartLocation(com.google.firebase.firestore.GeoPoint startLocation) {
-        this.startLocation = startLocation;
-    }
-
-    public com.google.firebase.firestore.GeoPoint getEndLocation() {
+    public GeoPoint getStartLocation() { return startLocation; }
+    public GeoPoint getEndLocation() {
         return endLocation;
     }
+    public String getUIDdriver() {
+        return UIDdriver;
+    }
+    public String getUIDrider(){
+        return UIDrider;
+    }
+    public String getStartAddress() {
+        return startAddress;
+    }
+    public String getEndAddress() {
+        return endAddress;
+    }
+    public String getDistanceText() {
+        return distanceText;
+    }
+    public String getDurationText() {
+        return durationText;
+    }
+    public Integer getDistanceValue() {
+        return distanceValue;
+    }
+    public Integer getDurationValue() {
+        return durationValue;
+    }
+    public String getRideStatus() {
+        return rideStatus;
+    }
+    public Double getRideCost() {
+        return rideCost;
+    }
 
-    public void setEndLocation(com.google.firebase.firestore.GeoPoint endLocation) {
+    public void setStatus(String rideStatus) {
+        this.rideStatus = rideStatus;
+    }
+    public void setStartLocation(GeoPoint startLocation) { this.startLocation = startLocation;}
+    public void setEndLocation(GeoPoint endLocation) {
         this.endLocation = endLocation;
     }
-
-    public String getDriverID() {
-        return driverID;
+    public void setUIDdriver(String UIDdriver) {
+        this.UIDdriver = UIDdriver;
     }
-
-    public void setDriverID(String driverID) {
-        this.driverID = driverID;
+    public void setUIDrider(String UIDrider) {
+        this.UIDrider = UIDrider;
+    }
+    public void setStartAddress(String startAddress) {
+        this.startAddress = startAddress;
+    }
+    public void setEndAddress(String endAddress) {
+        this.endAddress = endAddress;
+    }
+    public void setDistanceText(String distanceText) {
+        this.distanceText = distanceText;
+    }
+    public void setDistanceValue(Integer distanceValue) {
+        this.distanceValue = distanceValue;
+    }
+    public void setDurationText(String durationText) {
+        this.durationText = durationText;
+    }
+    public void setDurationValue(Integer durationValue) {
+        this.durationValue = durationValue;
+    }
+    public void setRideStatus(String rideStatus) {
+        this.rideStatus = rideStatus;
+    }
+    public void setRideCost(Double rideCost) {
+        this.rideCost = rideCost;
     }
 }
